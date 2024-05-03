1fc68ca12ace39c034c79bf10684f29106a74d1f
KOHJUNKET <1211210437@student.mmu.edu.my>
2024-05-03 16:48:46 +0800

    customer page

---
+++ Customer/header.php
@@ -0,0 +1,111 @@
+<header>
+<img src="./image/bee8187f8ec4798e571fdcee0b3d86df.png" class="image">
+<ul class="nav"> 
+    <li><a href="mainpage.html" class="a1">HOME</a></li>
+    <li>
+    <a href="menpage.html" class="a1">MEN</a>
+    <ul class="dropdown">
+        <li class="hover-me"><a href="">Shoes<i class='bx bx-chevron-right'></i></a>
+        <div class="dropdown2">
+            <ul>
+            <li><a href="">Nike</a></li>
+            <li><a href="">NewBalance</a></li>
+            <li><a href="">Adidas</a></li>
+            </ul>
+        </div>
+        </li>
+        <li class="hover-me"><a href="">Clothing<i class='bx bx-chevron-right'></i></a>
+            <div class="dropdown2">
+                <ul>
+                <li><a href="">Nike</a></li>
+                <li><a href="">NewBalance</a></li>
+                <li><a href="">Adidas</a></li>
+                </ul>
+            </div>
+        </li>
+        </ul>
+    </li>
+    <li>
+        <a href="menpage.html" class="a1">WOMAN</a>
+        <ul class="dropdown">
+            <li class="hover-me"><a href="">Shoes<i class='bx bx-chevron-right'></i></a>
+            <div class="dropdown2">
+                <ul>
+                <li><a href="">Nike</a></li>
+                <li><a href="">NewBalance</a></li>
+                <li><a href="">Adidas</a></li>
+                </ul>
+            </div>
+            </li>
+            <li class="hover-me"><a href="">Clothing<i class='bx bx-chevron-right'></i></a>
+                <div class="dropdown2">
+                    <ul>
+                    <li><a href="">Nike</a></li>
+                    <li><a href="">NewBalance</a></li>
+                    <li><a href="">Adidas</a></li>
+                    </ul>
+                </div>
+            </li>
+            </ul>
+        </li>
+        <li>
+            <a href="menpage.html" class="a1">KIDS</a>
+            <ul class="dropdown">
+                <li class="hover-me"><a href="">Shoes<i class='bx bx-chevron-right'></i></a>
+                <div class="dropdown2">
+                    <ul>
+                    <li><a href="">Nike</a></li>
+                    <li><a href="">NewBalance</a></li>
+                    <li><a href="">Adidas</a></li>
+                    </ul>
+                </div>
+                </li>
+                <li class="hover-me"><a href="">Clothing<i class='bx bx-chevron-right'></i></a>
+                    <div class="dropdown2">
+                        <ul>
+                        <li><a href="">Nike</a></li>
+                        <li><a href="">NewBalance</a></li>
+                        <li><a href="">Adidas</a></li>
+                        </ul>
+                    </div>
+                </li>
+                </ul>
+            </li>
+    <li>
+    <a href="" class="a1">BRANDS</a>
+    <ul class="dropdown">
+        <li><a href="">NIKE</a></li>
+        <li><a href="">NEW BALANCE</a></li>
+        <li><a href="">ADIDAS</a></li>
+    </ul>
+    </li>
+    <li><a href="" class="a1">LATEST</a></li>
+</ul>
+<div class="nav2">
+    <a href="">About Us</a>
+    <a href="">FAQ</a>
+    <a href="">Sign In</a>
+</div>
+<form>
+    <div class="search">
+        <span class="search-icon material-symbols-outlined">search</span>
+        <input class="search-input" type="search" placeholder="Search">
+    </div>
+</form>
+<div class="nav-icon-container">
+    <ul class="nav-icon">
+        <li>
+        <li><a href="#"><i class='bx bx-cart'></i></a></li>
+       <li><a href="#"><i class='bx bxs-heart' ></i></a></li>
+        <li>
+        <a href="#"><i class='bx bx-user' ></i></a>
+        <ul class="icon-dropdown">
+            <li><a href="">Sign in</a></li>
+            <li><a href="">Feedback</a></li>
+        </ul>
+        </li>
+    </li>
+    </ul>
+</div>
+</header>
+
