e0989e832817302a0a0a7a8e919dc2cbe1567c37
DanielTanNaiLe <1211209005@student.mmu.edu.my>
2024-05-03 18:08:05 +0800

    customer login

---
+++ Customer/customer login.php
@@ -0,0 +1,133 @@
+<?php
+    session_start();
+    include("dataconnection.php");
+
+    $error = '';
+    if(isset($_POST['submit'])){
+        if(empty($_POST['user_id']) || empty($_POST['user_password'])){
+            $error = "Username or Password is Invalid";
+        }
+        else{
+            $user_id = $_POST['user_id'];
+            $user_password = $_POST['user_password'];
+
+            include("dataconnection.php"); 
+
+            $db = mysqli_select_db($con, "ldksports");
+            $query = mysqli_query($conn, "SELECT * FROM user WHERE user_id ='$user_id' AND user_password = '$user_password'");
+            $row = mysqli_fetch_assoc($query);
+            $row2 = mysqli_num_rows($query);
+
+
+            if($row2 == 1){  
+            header("Location: landingafterlogin.php");
+            $_SESSION['u_id']= $user_id;
+            $_SESSION['u_name']= $row['user_name'];
+
+            }  
+            else  
+            {  
+            $error = "Username or Password is Invalid";
+            }  
+            
+            mysqli_close($conn);
+        }
+    }
+?>
+<!DOCTYPE.html>
+<html>
+<head>
+<title>Log In |LDK SPORTS</title>
+<link rel="icon" href="image/logo_img.jpg" type="image/x-icon">
+<style>
+    /***************** All ***********************/
+*{
+	box-sizing: border-box;
+}
+
+/******************Content***************/
+
+.login{
+    background-color:beige;
+    padding-top: 50pt;
+    padding-bottom: 200pt;
+}
+.overall{
+    border: 1px solid #f5f5f5;
+    text-align: center;
+    width: 60%;
+    margin-left: 20%;
+    height:320pt;
+    background-color: burlywood;
+    font-family: arial;
+}
+.image{
+    height: 320pt;
+    float: left;
+    width: 40%;
+}
+.detail h2{
+    text-align: center;
+}
+.detail {
+    text-align: center;
+    margin-top: 50pt;
+    font-size: 120%;
+}
+.submit {
+    background-color: #ffe7a4;
+    color: black;
+    padding: 14px 20px;
+    margin-top: 20pt;
+    border: none;
+    cursor: pointer;
+    border-radius: 8px;
+    font-weight: bold;
+    text-decoration: none;
+  }
+.submit:hover{
+    background-color: #A9A9A9;
+    color: white;
+    text-decoration: none;
+}
+.detail p{
+    font-size: 90%;
+}
+span{
+    color: red;
+}
+
+</style>
+</head>
+<body>
+
+
+    <form class="login" method="post" action="">
+        <div class="overall">
+            <img src="image/shop_img.jpg" class="image" >
+            <div class="detail">
+            <h2 style="font-weight:bold;">Log In</h2>
+            <h3>Welcome to LDK Sports</h3>
+            </br></br>
+
+                <label>Username : </label>
+                <input type="text" placeholder="Enter your username" name="user_id" id="user_id"required/>
+            </br></br>
+                <label>Password : </label>
+                <input type="password" placeholder="Enter your password" name="user_password" id="user_password" required/>
+            </br></br>
+            <a href="forgot_password.php">Forgot Password?</a>
+            </br>
+            <button type="submit" class="submit" name="submit">Log In</button>
+
+</br>
+                <span><?php echo $error;?></span>
+            </br>
+            <p>Don't have an account? <a href="customer register.html">Sign Up Here</a></p>
+            
+            </div>
+        </div>
+    </form>
+
+</body>
+</html>
---
+++ Customer/customer register.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Signup</title>
+    <link rel="icon" href="image/logo_img.jpg" type="image/x-icon">
+    <?php include("dataconnection.php"); ?>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    
+    <div class="img-cover2">
+        <img src="" class="image" >
+    </div>
+
+    <form class="registerfrm" method="POST" action="">
+    <div class = "overall">
+
+        <div class="img-cover">
+            <img src="image/Adidas Originals Wall.jpeg" class="image" >
+        </div>
+        
+    <div class="detail">
+        
+        <h1 style="font-weight:bold;">Register</h1>
+        <p> Fill in this form to register an account.</p>
+        
+        <div class="frmcontent">
+            <label>Username</label>
+            <input style="float: right; margin-right: 50pt;" type="text" placeholder="Enter Username" name="user_id" required>
+            <br/><br/>
+            <label>Fullname</label>
+            <input style="float: right; margin-right: 50pt;"type="text" placeholder="Enter Your Full Name" name="user_name" required>
+            <br/><br/>
+            <label>Phone Number </label>
+            <input style="float: right; margin-right: 50pt;"type="text" placeholder="Enter Phone Number" name="user_phone_number" required>
+            <br/><br/>
+            <label>Date of Birth</label>
+            <input style="float: right; margin-right: 65pt;" type="date" name="user_dob" value="" id="user_dob" required>
+            <br/><br/>
+            <label>Email </label>
+            <input style="float: right; margin-right: 50pt;"type="text" placeholder="Enter Email" name="user_email" required>
+            <br/><br/>
+            <label>Password </label>
+            <input style="float: right; margin-right: 50pt;"type="password" placeholder="Enter Password" name="user_password" required>
+            <br/><br/>
+            <label>Address </label>
+            <textarea style="float: right; margin-right: 50pt;" cols="23" rows="5" placeholder="Address" name="user_address" required></textarea>
+            <br/><br/>
+        </div>
+
+        <label style="margin-top: 30pt;">
+            <br/><br/>
+            <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+        </label>
+
+        <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+        <div class="clearfix">
+            
+            <button type="submit" class="signupbtn" name="signup" >Sign Up</button>
+        
+        </div>
+        <p>Already have an account? <a href="customer login.html">Login Here</a></p>
+    </div>
+</div>
+
+</form>
+    
+</body>
+</html>
+<?php
+	
+if(isset($_POST['register']))
+{ 
+    $user_id = $_POST['user_id'];
+	$user_name = $_POST['user_name'];
+    $user_password = $_POST['user_password'];
+    $user_dob = $_POST['user_dob'];
+    $user_phone_number = $_POST['user_phone_number'];
+    $user_email = $_POST['user_email'];
+    $user_address=$_POST['user_address'];
+    
+	$result = mysqli_query($conn,"INSERT INTO user(user_id, user_name, user_dob, user_phone_number, user_email,user_password, user_address)VALUES('$user_id','$user_name','$user_dob','$user_phone_number','$user_email','$user_password','$user_address')");
+
+    if($result)
+	{
+?>
+<script> 
+alert("Thank you for register!");
+location.replace("customer login.php");
+</script>
+<?php
+    }
+}
+
+?>
