c419936c07849bdc0ceccdfb5364ce6b428c1f2a
DanielTanNaiLe <1211209005@student.mmu.edu.my>
2024-05-04 23:58:36 +0800

    forget

--- Customer/send-password-reset.php
+++ Customer/send-password-reset.php
@@ -35,6 +35,17 @@ if ($result === false) {
 
 if ($stmt->affected_rows) {
     // Your code for sending the email
+    $mail = require __DIR__ . "/mailer.php";
+
+    $mail->setFrom("noreply@example.com");
+    $mail->addAddress($user_email);
+    $mail->Subject = "Password Reset";
+    $mail->Body = <<<END
+
+    Click <a href="http://example.com/reset-password.php?token=$token">here</a> 
+    to reset your password.
+
+    END;
     echo "Message sent, please check your inbox.";
 } else {
     echo "Message couldnt be sent.";
