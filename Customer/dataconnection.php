43d1332ed2016d667718191da6de1095fb154e1e
DanielTanNaiLe <1211209005@student.mmu.edu.my>
2024-05-04 18:24:35 +0800

    forget

--- Customer/dataconnection.php
+++ Customer/dataconnection.php
@@ -8,10 +8,10 @@ $dbname = "ldksports";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-return $conn; // Return the MySQLi object
+return $conn;
+
 ?>
