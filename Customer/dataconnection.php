5293a6815455036286f1c4c672eda8bc4ade7656
DanielTanNaiLe <1211209005@student.mmu.edu.my>
2024-05-04 18:21:40 +0800

    forget

--- Customer/dataconnection.php
+++ Customer/dataconnection.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "ldkrestaurant";
+$dbname = "ldksports";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
