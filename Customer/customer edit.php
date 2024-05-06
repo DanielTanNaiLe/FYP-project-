ff2d02202173185ce5a512c129896258b874e14c
DanielTanNaiLe <1211209005@student.mmu.edu.my>
2024-05-07 01:43:53 +0800

    edit

--- Customer/customer edit.php
+++ Customer/customer edit.php
@@ -2,35 +2,37 @@
 session_start();
 include("dataconnection.php");
 
-if(isset($_GET['update'])) {
+if(isset($_GET['update']))
+{
     $user_id = $_GET['user_id'];
     $result = mysqli_query($conn, "SELECT * FROM users WHERE user_id='$user_id'");
     
-    if($result) {
-        $row = mysqli_fetch_assoc($result);
+    if($result)
+    {
+        $_SESSION = mysqli_fetch_assoc($result);
 ?>
-<!DOCTYPE html>
+<!DOCTYPE.html>
 <html>
 <head>
-<title>My Account | G5 Bookstore</title>
+<title>My Account |LDK SPORTS</title>
 <link rel="icon" href="../Image/G5_LOGO_PNG_TITLE.png" type="image/x-icon">
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="general_design.css">
 
 <style>
-/***************** All ***********************/
-* {
-    box-sizing: border-box;
+    /***************** All ***********************/
+*{
+	box-sizing: border-box;
 }
 
 /*****Content****/
-.content {
+.content{
     background-color: #f5f5f5;
     text-align: center;
     margin-top: -50px;
+    
 }
-
-.left {
+.left{
     background-color: #A9A9A9;
     height: 600px;
     width: 20%;
@@ -39,8 +41,7 @@ if(isset($_GET['update'])) {
     margin-left: 4%;
     padding-top: 50px;
 }
-
-.right {
+.right{
     border: 1px solid white;
     height: 600px;
     width: 70%;
@@ -51,60 +52,51 @@ if(isset($_GET['update'])) {
     padding-top: 30px;
     text-align: left;
 }
-
-.right h2 {
+.right h2{
     text-align: left;
     font-family: garamond;
 }
-
-.right p label {
-    width: 150px;
+.right p label{
+    width:150px;
 }
-
 .avatar {
     vertical-align: middle;
     width: 100px;
     height: 100px;
     border-radius: 50%;
-}
-
-.menu {
+  }
+.menu{
     margin-top: 50px;
     text-align: center;
 }
-
-.menu ul {
+.menu ul{
     list-style-type: none;
     text-align: center;
     width: 80%;
 }
-
-.menu li {
+.menu li{
     font-size: 20px;
     height: 50px;
 }
 
-.menu a {
-    color: white;
+.menu a{
+    color:white;
 }
-
-.menu li a:hover {
+.menu li a:hover{
     background-color: #f5f5f5;
     text-decoration: none;
     color: black;
 }
-
-.right h3 {
+.right h3{
     text-align: left;
     border-bottom: 1px solid black;
 }
-
-.right p {
+.right p{
     font-family: Verdana;
     font-size: 16px;
+    
 }
-
-.save {
+.save{
     background-color: #ffe7a4;
     color: black;
     padding: 14px 20px;
@@ -114,8 +106,7 @@ if(isset($_GET['update'])) {
     border-radius: 8px;
     font-weight: bold;
 }
-
-.save:hover {
+.save:hover{
     background-color: #A9A9A9;
     color: white;
 }
@@ -125,58 +116,66 @@ if(isset($_GET['update'])) {
 
 <body>
 
+    
 <form class="content">
     <div class="left">
         <img src="../Image/avatar.png" alt="Avatar" class="avatar">
-        <div class="menu">
-            <ul>
-                <li><a href="landingafterlogin.php">Profile</a></li>
-                <li><a href="#">Shopping Cart</a></li>
-                <li><a href="logout.php" name="logout">Log Out</a></li>
-            </ul>
-        </div>
+    </br>
+    <div class="menu">
+        <ul>
+            <li><a href="landingafterlogin.php">Profile</a>
+            </li>
+            <li><a href="#">Shopping Cart</a>
+            </li>
+            <li><a href="logout.php" name="logout">Log Out</a>
+            </li>
+        </ul>
+    </div>
     </div>
 </form>
 
-<div class="right">
-    <h2>Edit</h2>
-    <h3>Personal Information</h3>
-    <form name="update" method="post" action="">
-        <p><label>Name:</label><input type="text" name="user_name" size="40" placeholder="Please enter your name" id="user_name" value="<?php echo htmlspecialchars($row['user_name']); ?>"></p>
-        <p><label>Date of Birth:</label><input type="date" name="user_dob" value="<?php echo htmlspecialchars($row['user_dob']); ?>" id="user_dob"></p>
-        <p><label>Phone Number:</label><input type="text" name="user_phone_number" size="40" placeholder="Please enter your phone number" id="user_phone_number" value="<?php echo htmlspecialchars($row['user_phone_number']); ?>"></p>
-        <p><label>Email:</label><input type="text" name="user_email" size="40" placeholder="Please enter your email" id="user_email" value="<?php echo htmlspecialchars($row['user_email']); ?>"></p>
-        <p><label>Address:</label><textarea cols="40" rows="3" name="user_address" id="user_address"><?php echo htmlspecialchars($row['user_address']); ?></textarea></p>
-        <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
-        <input type="hidden" name="user_password" value="<?php echo $row['user_password']; ?>">
-        <p><button style="margin-top: 40pt;" class="save" name="savebtn">Save</button></p>
-    </form>
-</div>
 
+<div class="right">
+        <h2>Edit </h2>
+        <h3>Personal Information</h3>
+        <form name="update" method="post" action="">
+        <p><label>Name:</label><input  type="text" name="user_name" size="40" placeholder="Please enter your name" id="user_name" value="<?php echo $_SESSION['user_name'];?>">
+        
+        <p><label>Date of Birth:</label><input  type="date" name="user_dob" value="<?php echo $_SESSION['user_dob'];?>" id="user_dob">
+        
+        <p><label>Phone Number:</label><input  type="text" name="user_phone_number" size="40" placeholder="Please enter your phone number" id="user_phone_number" value="<?php echo $_SESSION['user_phone_number'];?>">
+
+        <p><label>Email:</label><input  type="text" name="user_email" size="40" placeholder="Please enter your email" id="user_email" value="<?php echo $_SESSION['user_email'];?>">
+
+        <p><label>Address:</label><textarea  cols="40" rows="3" name="user_address" id="user_address"> <?php echo $_SESSION['user_address'];?></textarea>
+        <input type="hidden" name="user_password" value="<?php echo $_SESSION['user_password'];?>">
+        <input type="hidden" name="user_id" value="<?php echo $_SESSION['user_id'];?>">
+
+        <p><button style="margin-top: 40pt;" class="save" name="savebtn">Save</button>
+        </form>
+</div>
 </body>
 </html>
-
 <?php
-    }
-}
+if(isset($_POST['savebtn']))
+{
+    $user_id=$_POST['user_id'];
+    $user_name=$_POST['user_name'];
+    $user_password=$_POST['user_password'];
+    $user_dob=$_POST['user_dob'];
+    $user_phone_number=$_POST['user_phone_number'];
+    $user_email=$_POST['user_email'];
+    $user_address=$_POST['user_address'];
 
-if(isset($_POST['savebtn'])) {
-    $user_id = $_POST['user_id'];
-    $user_name = $_POST['user_name'];
-    $user_password = $_POST['user_password'];
-    $user_dob = $_POST['user_dob'];
-    $user_phone_number = $_POST['user_phone_number'];
-    $user_email = $_POST['user_email'];
-    $user_address = $_POST['user_address'];
-
-    $run = mysqli_query($conn, "UPDATE users SET user_name='$user_name', user_password='$user_password', user_dob='$user_dob', user_phone_number='$user_phone_number', user_email='$user_email', user_address='$user_address' WHERE user_id='$user_id'");
+    $run =mysqli_query($conn, "UPDATE user SET user_name='$user_name', user_password='$user_password', user_dob='$user_dob', user_phone_number='$user_phone_number', user_email='$user_email', user_address='$user_address' WHERE user_id=$user_id");
     
-    if($run) {
-        ?>
-        <script>alert("Admin updated"); location.replace("landingafterlogin.php"); </script>
-        <?php
-    } else {
-        echo "Error: " . mysqli_error($conn);
-    }
+    
+    ?>
+    <script>alert("Admin updated");
+    location.replace("landingafterlogin.php"); </script>
+    </script>
+    <?php
+
 }
+}}
 ?>
