6d52a17c2906e0716f087aa7a8b2e17bd80048cc
DanielTanNaiLe <1211209005@student.mmu.edu.my>
2024-05-07 02:39:21 +0800

    edit

--- Customer/customer edit.php
+++ Customer/customer edit.php
@@ -6,14 +6,6 @@ include("dataconnection.php");
 if(isset($_SESSION['u_id']) && isset($_GET['update'])) {
     $user_id = $_SESSION['u_id'];
 
-    // Fetch user data from the session
-    $user_name = $_SESSION['u_name'];
-    $user_dob = $_SESSION['u_dob'];
-    $user_phone_number = $_SESSION['u_phone_number'];
-    $user_email = $_SESSION['u_email'];
-    $user_address = $_SESSION['u_address'];
-    $user_password = $_SESSION['user_password'];
-
     // Handle form submission
     if(isset($_POST['savebtn'])) {
         // Retrieve updated information from the form
@@ -32,14 +24,14 @@ if(isset($_SESSION['u_id']) && isset($_GET['update'])) {
         mysqli_stmt_bind_param($stmt, 'ssssssi', $user_name, $user_dob, $user_password, $user_phone_number, $user_email, $user_address, $user_id);
         mysqli_stmt_execute($stmt);
         
-        if(mysqli_stmt_execute($stmt)) {
+        if(mysqli_stmt_affected_rows($stmt) > 0) {
             // Update session variables with the new data
             $_SESSION['u_name'] = $user_name;
             $_SESSION['u_dob'] = $user_dob;
             $_SESSION['u_phone_number'] = $user_phone_number;
             $_SESSION['u_email'] = $user_email;
             $_SESSION['u_address'] = $user_address;
-        
+
             // Redirect user to profile page after successful update
             header("Location: landingafterlogin.php");
             exit();
@@ -47,6 +39,14 @@ if(isset($_SESSION['u_id']) && isset($_GET['update'])) {
             echo "Error updating record: " . mysqli_error($conn);
         }
     }
+
+    // Fetch user data from the session
+    $user_name = $_SESSION['u_name'];
+    $user_dob = $_SESSION['u_dob'];
+    $user_phone_number = $_SESSION['u_phone_number'];
+    $user_email = $_SESSION['u_email'];
+    $user_address = $_SESSION['u_address'];
+    $user_password = $_SESSION['user_password'];
 ?>
 
 <!DOCTYPE html>
@@ -59,98 +59,95 @@ if(isset($_SESSION['u_id']) && isset($_GET['update'])) {
 
     <style>
         /* Your CSS styles here */
-/***************** All ***********************/
-*{
-	box-sizing: border-box;
-}
+        /***************** All ***********************/
+        *{
+            box-sizing: border-box;
+        }
 
-/*****Content****/
-.content{
-    background-color: #f5f5f5;
-    text-align: center;
-    margin-top: -50px;
-    
-}
-.left{
-    background-color: #A9A9A9;
-    height: 600px;
-    width: 20%;
-    margin-top: 100px;
-    float: left;
-    margin-left: 4%;
-    padding-top: 50px;
-}
-.right{
-    border: 1px solid white;
-    height: 600px;
-    width: 70%;
-    margin-top: 100px;
-    float: right;
-    margin-right: 4%;
-    padding-left: 50px;
-    padding-top: 30px;
-    text-align: left;
-}
-.right h2{
-    text-align: left;
-    font-family: garamond;
-}
-.right p label{
-    width:150px;
-}
-.avatar {
-    vertical-align: middle;
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-  }
-.menu{
-    margin-top: 50px;
-    text-align: center;
-}
-.menu ul{
-    list-style-type: none;
-    text-align: center;
-    width: 80%;
-}
-.menu li{
-    font-size: 20px;
-    height: 50px;
-}
+        /*****Content****/
+        .content{
+            background-color: #f5f5f5;
+            text-align: center;
+            margin-top: -50px;
+        }
+        .left{
+            background-color: #A9A9A9;
+            height: 600px;
+            width: 20%;
+            margin-top: 100px;
+            float: left;
+            margin-left: 4%;
+            padding-top: 50px;
+        }
+        .right{
+            border: 1px solid white;
+            height: 600px;
+            width: 70%;
+            margin-top: 100px;
+            float: right;
+            margin-right: 4%;
+            padding-left: 50px;
+            padding-top: 30px;
+            text-align: left;
+        }
+        .right h2{
+            text-align: left;
+            font-family: garamond;
+        }
+        .right p label{
+            width:150px;
+        }
+        .avatar {
+            vertical-align: middle;
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+        }
+        .menu{
+            margin-top: 50px;
+            text-align: center;
+        }
+        .menu ul{
+            list-style-type: none;
+            text-align: center;
+            width: 80%;
+        }
+        .menu li{
+            font-size: 20px;
+            height: 50px;
+        }
 
-.menu a{
-    color:white;
-}
-.menu li a:hover{
-    background-color: #f5f5f5;
-    text-decoration: none;
-    color: black;
-}
-.right h3{
-    text-align: left;
-    border-bottom: 1px solid black;
-}
-.right p{
-    font-family: Verdana;
-    font-size: 16px;
-    
-}
-.save{
-    background-color: #ffe7a4;
-    color: black;
-    padding: 14px 20px;
-    margin-top: 20pt;
-    border: none;
-    cursor: pointer;
-    border-radius: 8px;
-    font-weight: bold;
-}
-.save:hover{
-    background-color: #A9A9A9;
-    color: white;
-}
-</style>
-
+        .menu a{
+            color:white;
+        }
+        .menu li a:hover{
+            background-color: #f5f5f5;
+            text-decoration: none;
+            color: black;
+        }
+        .right h3{
+            text-align: left;
+            border-bottom: 1px solid black;
+        }
+        .right p{
+            font-family: Verdana;
+            font-size: 16px;
+        }
+        .save{
+            background-color: #ffe7a4;
+            color: black;
+            padding: 14px 20px;
+            margin-top: 20pt;
+            border: none;
+            cursor: pointer;
+            border-radius: 8px;
+            font-weight: bold;
+        }
+        .save:hover{
+            background-color: #A9A9A9;
+            color: white;
+        }
+    </style>
 </head>
 <body>
 
