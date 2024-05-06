7a4dda009c42aeafa082ea058d8f3c8e118d75ff
DanielTanNaiLe <1211209005@student.mmu.edu.my>
2024-05-06 22:38:45 +0800

    forget

--- Customer/send-password-reset.php
+++ Customer/send-password-reset.php
@@ -41,7 +41,7 @@ if ($stmt->affected_rows) {
     $mail->addAddress($user_email);
     $mail->Subject = "Password Reset";
     $mail->Body = <<<END
-    Click <a href="http://localhost/FYP-project--4/Customer/reset-password.php?token=$token">here</a> 
+    Click <a href="http://localhost/FYP-project--5/Customer/reset-password.php?token=$token">here</a> 
     to reset your password.
  
     END;
