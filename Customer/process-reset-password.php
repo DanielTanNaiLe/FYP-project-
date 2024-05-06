c6ff3b65a3fa5a835635c84626fdd0b1bf3d2552
DanielTanNaiLe <1211209005@student.mmu.edu.my>
2024-05-06 22:38:24 +0800

    forget

--- Customer/process-reset-password.php
+++ Customer/process-reset-password.php
@@ -20,22 +20,22 @@ $result = $stmt->get_result();
 $user = $result->fetch_assoc();
 
 if ($user === null) {
-    die("token not found");
+    die("Token not found");
 }
 
 if (strtotime($user["reset_token_expires_at"]) <= time()) {
-    die("token has expired");
+    die("Token has expired");
 }
 
 if (strlen($_POST["password"]) < 8) {
     die("Password must be at least 8 characters");
 }
 
-if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
+if (!preg_match("/[a-z]/i", $_POST["password"])) {
     die("Password must contain at least one letter");
 }
 
-if ( ! preg_match("/[0-9]/", $_POST["password"])) {
+if (!preg_match("/[0-9]/", $_POST["password"])) {
     die("Password must contain at least one number");
 }
 
@@ -45,11 +45,11 @@ if ($_POST["password"] !== $_POST["password_confirmation"]) {
 
 $user_password = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-$sql = "UPDATE users
-        SET user_password = ?,
-            reset_token_hash = NULL,
-            reset_token_expires_at = NULL
-        WHERE id = ?";
+$sql_update = "UPDATE users
+               SET user_password = ?,
+                   reset_token_hash = NULL,
+                   reset_token_expires_at = NULL
+               WHERE id = ?";
 
 $stmt_update = $mysqli->prepare($sql_update);
 
