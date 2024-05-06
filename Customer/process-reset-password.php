af1b84a2398514ad710a86ada3a4069e1f63bbac
DanielTanNaiLe <1211209005@student.mmu.edu.my>
2024-05-06 21:46:10 +0800

    forget

--- Customer/process-reset-password.php
+++ Customer/process-reset-password.php
@@ -27,19 +27,19 @@ if (strtotime($user["reset_token_expires_at"]) <= time()) {
     die("token has expired");
 }
 
-if (strlen($_POST["user_password"]) < 8) {
+if (strlen($_POST["password"]) < 8) {
     die("Password must be at least 8 characters");
 }
 
-if ( ! preg_match("/[a-z]/i", $_POST["user_password"])) {
+if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
     die("Password must contain at least one letter");
 }
 
-if ( ! preg_match("/[0-9]/", $_POST["user_password"])) {
+if ( ! preg_match("/[0-9]/", $_POST["password"])) {
     die("Password must contain at least one number");
 }
 
-if ($_POST["user_password"] !== $_POST["password_confirmation"]) {
+if ($_POST["password"] !== $_POST["password_confirmation"]) {
     die("Passwords must match");
 }
 
