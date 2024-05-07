d546aa0f1378f173c137ecef79c5dc221db57432
DanielTanNaiLe <1211209005@student.mmu.edu.my>
2024-05-07 18:07:36 +0800

    login

--- Customer/customer login.php
+++ Customer/customer login.php
@@ -1,6 +1,6 @@
 <?php
 
- require '../admin_panel/config/dbconnect.php';
+ include '../admin_panel/config/dbconnect.php';
 session_start();
 
 if(isset($_POST['submit'])){
@@ -13,7 +13,7 @@ if(isset($_POST['submit'])){
    if(mysqli_num_rows($select) > 0){
       $row = mysqli_fetch_assoc($select);
       $_SESSION['user_id'] = $row['id'];
-      header('location:landingafterlogin.php');
+      header('location:http://localhost/FYP-project--3/landingafterlogin/.php');
    }else{
       $message[] = 'incorrect email or password!';
    }
