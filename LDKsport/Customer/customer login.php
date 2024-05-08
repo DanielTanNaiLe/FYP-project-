475f914fbf64645e198f61ff8043faa0a784da94
DanielTanNaiLe <1211209005@student.mmu.edu.my>
2024-05-09 01:23:19 +0800

    login

--- LDKsport/Customer/customer login.php
+++ LDKsport/Customer/customer login.php
@@ -52,7 +52,7 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <script src="assets/js/sweetalert.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
    <title>login</title>
 
    <!-- custom css file link  -->
