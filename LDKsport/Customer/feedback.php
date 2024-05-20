4f9082ca467d01974ffc22c5e43141bfe68c0bee
DanielTanNaiLe <1211209005@student.mmu.edu.my>
2024-05-20 19:08:43 +0800

    f

--- LDKsport/Customer/feedback.php
+++ LDKsport/Customer/feedback.php
@@ -12,7 +12,7 @@ include("header.php");
 <title>Feedback</title>
 <link rel="icon" href="image/logo.png" type="image/x-icon">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-<link rel="stylesheet"href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+<link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
 <link rel="stylesheet" href="general.css">
 
 <style>
@@ -28,19 +28,17 @@ body {
     font-family: Arial, sans-serif;
 }
 .txt-center {
-    height: 400px;
     text-align: center;
-    margin-top: 250px; /* Adjust this value based on the height of your header */
+    margin-top: 50px; /* Adjust this value based on your header height */
 }
 .txt-center h2 {
-    margin-top: 0px;
+    margin-top: 0;
     padding-left: 20px;
-    padding-bottom: 0px;
+    padding-bottom: 0;
 }
 .txt-center h4 {
     padding-left: 20px;
-    padding-top: 0px;
-    font-style: oblique;
+    font-style: italic;
     color: red;
 }
 textarea {
@@ -59,7 +57,6 @@ textarea {
     border: none;
 }
 .clear {
-    float: none;
     clear: both;
 }
 .hide {
@@ -104,51 +101,29 @@ textarea {
 </head>
 
 <body>
-<?php
-if (isset($_SESSION["user_id"])) {
-    $user_id = $_SESSION["user_id"];
-    $result = mysqli_query($conn, "SELECT * FROM users WHERE user_id=$user_id"); 
-    $count = mysqli_num_rows($result); // used to count number of rows
-    
-    if ($count > 0) {
-        $row = mysqli_fetch_array($result);
-       
-        ?>
-        <div class="content">
-            <div class="txt-center">
-                <h2>How do you think of our online shop?</h2>
-                <h4>Please rate and write down your review for us to have a better improvement. Thank you!!</h4>
-                <form name="ratingfrm" method="post" action="">
-                    <div class="rating" name="feedback_rating">
-                        <input id="star5" name="star" type="radio" value="5" class="radio-btn hide" />
-                        <label for="star5">☆</label>
-                        <input id="star4" name="star" type="radio" value="4" class="radio-btn hide" />
-                        <label for="star4">☆</label>
-                        <input id="star3" name="star" type="radio" value="3" class="radio-btn hide" />
-                        <label for="star3">☆</label>
-                        <input id="star2" name="star" type="radio" value="2" class="radio-btn hide" />
-                        <label for="star2">☆</label>
-                        <input id="star1" name="star" type="radio" value="1" class="radio-btn hide" />
-                        <label for="star1">☆</label>
-                        <div class="clear"></div>
-                    </div>
-                    <textarea rows="5" cols="49" name="feedback_comment" placeholder="Please leave your comments here..."></textarea>
-                    <input type="submit" name="save" value="submit"/>
-                </form>
+<div class="content">
+    <div class="txt-center">
+        <h2>How do you think of our online shop?</h2>
+        <h4>Please rate and write down your review for us to have a better improvement. Thank you!!</h4>
+        <form name="ratingfrm" method="post" action="">
+            <div class="rating" name="feedback_rating">
+                <input id="star5" name="star" type="radio" value="5" class="radio-btn hide" />
+                <label for="star5">☆</label>
+                <input id="star4" name="star" type="radio" value="4" class="radio-btn hide" />
+                <label for="star4">☆</label>
+                <input id="star3" name="star" type="radio" value="3" class="radio-btn hide" />
+                <label for="star3">☆</label>
+                <input id="star2" name="star" type="radio" value="2" class="radio-btn hide" />
+                <label for="star2">☆</label>
+                <input id="star1" name="star" type="radio" value="1" class="radio-btn hide" />
+                <label for="star1">☆</label>
+                <div class="clear"></div>
             </div>
-        </div>
-        <?php
-        include("footer.php");
-    }
-} else {
-    ?>
-    <script>
-        alert("Please log in to your account for response. Thank you");
-        location.replace("customer login.php");
-    </script>
-    <?php
-}
-?>
+            <textarea rows="5" cols="49" name="feedback_comment" placeholder="Please leave your comments here..."></textarea>
+            <input type="submit" name="save" value="submit"/>
+        </form>
+    </div>
+</div>
 </body>
 </html>
 
