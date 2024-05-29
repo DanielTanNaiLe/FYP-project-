c4b8f2ab959e4baa9401c5ba4caab1d52c2b8b45
DanielTanNaiLe <1211209005@student.mmu.edu.my>
2024-05-29 13:01:27 +0800

    products

--- LDKsport/Customer/header.php
+++ LDKsport/Customer/header.php
@@ -10,126 +10,122 @@ if(isset($_SESSION['user_id'])) {
 ?>
 
 <header>
-<img src="./image/bee8187f8ec4798e571fdcee0b3d86df.png" class="image">
-
+    <img src="./image/bee8187f8ec4798e571fdcee0b3d86df.png" class="image">
     <ul class="nav">
         <li><a href="mainpage.php" class="a1">HOME</a></li>
-<li>
-    <a href="menpage.php" class="a1">MEN</a>
-    <ul class="dropdown">
-     <li class="hover-me"><a href="">Shoes<i class='bx bx-chevron-right'></i></a>
-      <div class="dropdown2">
-     <ul>
-      <li><a href="">Nike</a></li>
-       <li><a href="">NewBalance</a></li>
-          <li><a href="">Adidas</a></li>
-          </ul>
-       </div>
-      </li>
-       <li class="hover-me"><a href="">Clothing<i class='bx bx-chevron-right'></i></a>
-        <div class="dropdown2">
-        <ul>
-          <li><a href="">Nike</a></li>
-          <li><a href="">NewBalance</a></li>
-           <li><a href="">Adidas</a></li>
-           </ul>
-        </div>
-        </li>
-    </ul>
-</li>
-<li>
-    <a href="girlpage.php" class="a1">WOMAN</a>
-    <ul class="dropdown">
-     <li class="hover-me"><a href="">Shoes<i class='bx bx-chevron-right'></i></a>
-      <div class="dropdown2">
-     <ul>
-      <li><a href="">Nike</a></li>
-       <li><a href="">NewBalance</a></li>
-          <li><a href="">Adidas</a></li>
-          </ul>
-       </div>
-      </li>
-       <li class="hover-me"><a href="">Clothing<i class='bx bx-chevron-right'></i></a>
-        <div class="dropdown2">
-        <ul>
-          <li><a href="">Nike</a></li>
-          <li><a href="">NewBalance</a></li>
-           <li><a href="">Adidas</a></li>
-           </ul>
-        </div>
-        </li>
-    </ul>
-</li>
-<li>
-    <a href="kidpage.php" class="a1">KIDS</a>
-    <ul class="dropdown">
-     <li class="hover-me"><a href="">Shoes<i class='bx bx-chevron-right'></i></a>
-      <div class="dropdown2">
-     <ul>
-      <li><a href="">Nike</a></li>
-       <li><a href="">NewBalance</a></li>
-          <li><a href="">Adidas</a></li>
-          </ul>
-       </div>
-      </li>
-       <li class="hover-me"><a href="">Clothing<i class='bx bx-chevron-right'></i></a>
-        <div class="dropdown2">
-        <ul>
-          <li><a href="">Nike</a></li>
-          <li><a href="">NewBalance</a></li>
-           <li><a href="">Adidas</a></li>
-           </ul>
-        </div>
-        </li>
-    </ul>
-</li>
-    <li>
-    <a href="" class="a1">BRANDS</a>
-    <ul class="dropdown">
-        <li><a href="">NIKE</a></li>
-        <li><a href="">NEW BALANCE</a></li>
-        <li><a href="">ADIDAS</a></li>
-    </ul>
-    </li>
-    <li><a href="order.php" class="a1">ORDER</a></li>
-</ul>
-<div class="nav2">
-    <a href="aboutus.php">About Us</a>
-    <a href="FAQ.php">FAQ</a>
-    <?php if($isLoggedIn): ?>
-            <a href="customer logout.php">Log out</a>
-        <?php else: ?>
-            <a href="customer login.php">Sign In</a>
-        <?php endif; ?>
-</div>
-<form>
-    <div class="search">
-        <span class="search-icon material-symbols-outlined">search</span>
-        <input class="search-input" type="search" placeholder="Search">
-    </div>
-</form>
-<div class="nav-icon-container">
-    <ul class="nav-icon">
-    <?php if($isLoggedIn): ?>
         <li>
-        <li><a href="Addtocart.php"><i class='bx bx-cart'></i></a></li>
-       <li><a href="wishlist.php"><i class='bx bxs-heart' ></i></a></li>
-                <li>
-                    <a href="landingafterlogin.php"><i class='bx bx-user' ></i></a>
-                    <ul class="icon-dropdown">
-                        <li><a href="customer logout.php">Log out</a></li>
-                        <li><a href="feedback.php">Feedback</a></li>
-        </ul>
-        </li>
-        <?php else: ?>
-                <li>
-                    <a href="Addtocart.php"><i class='bx bx-cart'></i></a>
+            <a href="menpage.php" class="a1">MEN</a>
+            <ul class="dropdown">
+                <li class="hover-me"><a href="">Shoes<i class='bx bx-chevron-right'></i></a>
+                    <div class="dropdown2">
+                        <ul>
+                            <li><a href="">Nike</a></li>
+                            <li><a href="">NewBalance</a></li>
+                            <li><a href="">Adidas</a></li>
+                        </ul>
+                    </div>
                 </li>
-                <li><a href="wishlist.php"><i class='bx bxs-heart' ></i></a></li>
+                <li class="hover-me"><a href="">Clothing<i class='bx bx-chevron-right'></i></a>
+                    <div class="dropdown2">
+                        <ul>
+                            <li><a href="">Nike</a></li>
+                            <li><a href="">NewBalance</a></li>
+                            <li><a href="">Adidas</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="girlpage.php" class="a1">WOMAN</a>
+            <ul class="dropdown">
+                <li class="hover-me"><a href="">Shoes<i class='bx bx-chevron-right'></i></a>
+                    <div class="dropdown2">
+                        <ul>
+                            <li><a href="">Nike</a></li>
+                            <li><a href="">NewBalance</a></li>
+                            <li><a href="">Adidas</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="hover-me"><a href="">Clothing<i class='bx bx-chevron-right'></i></a>
+                    <div class="dropdown2">
+                        <ul>
+                            <li><a href="">Nike</a></li>
+                            <li><a href="">NewBalance</a></li>
+                            <li><a href="">Adidas</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="kidpage.php" class="a1">KIDS</a>
+            <ul class="dropdown">
+                <li class="hover-me"><a href="">Shoes<i class='bx bx-chevron-right'></i></a>
+                    <div class="dropdown2">
+                        <ul>
+                            <li><a href="">Nike</a></li>
+                            <li><a href="">NewBalance</a></li>
+                            <li><a href="">Adidas</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="hover-me"><a href="">Clothing<i class='bx bx-chevron-right'></i></a>
+                    <div class="dropdown2">
+                        <ul>
+                            <li><a href="">Nike</a></li>
+                            <li><a href="">NewBalance</a></li>
+                            <li><a href="">Adidas</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="products.php" class="a1">BRANDS</a>
+            <ul class="dropdown">
+                <li><a href="products.php?brand=NIKE">NIKE</a></li>
+                <li><a href="products.php?brand=NEW BALANCE">NEW BALANCE</a></li>
+                <li><a href="products.php?brand=ADIDAS">ADIDAS</a></li>
+            </ul>
+        </li>
+        <li><a href="order.php" class="a1">ORDER</a></li>
+    </ul>
+    <div class="nav2">
+        <a href="aboutus.php">About Us</a>
+        <a href="FAQ.php">FAQ</a>
+        <?php if($isLoggedIn): ?>
+            <a href="customer_logout.php">Log out</a>
+        <?php else: ?>
+            <a href="customer_login.php">Sign In</a>
+        <?php endif; ?>
+    </div>
+    <form>
+        <div class="search">
+            <span class="search-icon material-symbols-outlined">search</span>
+            <input class="search-input" type="search" placeholder="Search">
+        </div>
+    </form>
+    <div class="nav-icon-container">
+        <ul class="nav-icon">
+            <?php if($isLoggedIn): ?>
+                <li><a href="Addtocart.php"><i class='bx bx-cart'></i></a></li>
+                <li><a href="wishlist.php"><i class='bx bxs-heart'></i></a></li>
                 <li>
-                    <a href="#"><i class='bx bx-user' ></i></a>
+                    <a href="landingafterlogin.php"><i class='bx bx-user'></i></a>
                     <ul class="icon-dropdown">
-                        <li><a href="customer login.php">Log in</a></li>
+                        <li><a href="customer_logout.php">Log out</a></li>
+                        <li><a href="feedback.php">Feedback</a></li>
+                    </ul>
+                </li>
+            <?php else: ?>
+                <li><a href="Addtocart.php"><i class='bx bx-cart'></i></a></li>
+                <li><a href="wishlist.php"><i class='bx bxs-heart'></i></a></li>
+                <li>
+                    <a href="#"><i class='bx bx-user'></i></a>
+                    <ul class="icon-dropdown">
+                        <li><a href="customer_login.php">Log in</a></li>
                         <li><a href="feedback.php">Feedback</a></li>
                     </ul>
                 </li>
@@ -137,4 +133,5 @@ if(isset($_SESSION['user_id'])) {
         </ul>
     </div>
 </header>
+
 
--- LDKsport/Customer/products.php
+++ LDKsport/Customer/products.php
@@ -1,5 +1,12 @@
 <?php
-    require '../admin_panel/config/dbconnect.php';
+require '../admin_panel/config/dbconnect.php';
+$brandFilter = isset($_GET['brand']) ? $_GET['brand'] : '';
+
+$query = "SELECT * FROM product";
+if ($brandFilter) {
+    $query .= " WHERE brand_id = (SELECT brand_id FROM brand WHERE brand_name = '$brandFilter')";
+}
+$result = mysqli_query($conn, $query);
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,32 +15,34 @@
     <link rel="stylesheet" href="general.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
-   
+    <style>
+        body{
+            padding: 80px;
+        }
+    </style>
 </head>
 <body>
-    <?php include("header.php"); 
-    $result = mysqli_query($conn, "SELECT * FROM product");
-    if (mysqli_num_rows($result) > 0) {
-        ?>
-    <div class="subtitle_1"><h1>Shoes</h1></div>
+    <?php include("header.php"); ?>
+    <div class="subtitle_1"><h1><?= $brandFilter ? ucfirst($brandFilter) . ' Products' : 'All Products' ?></h1></div>
     <div class="listproduct">
         <?php
-        while ($row = mysqli_fetch_assoc($result)) {
-            ?>
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+        ?>
         <div class="item">
             <img src="../uploads/<?= $row['product_image'] ?>" alt="">
-            <h2><?=$row["product_name"]?></h2>
-            <div class="price"><?=$row["price"]?></div>
+            <h2><?= $row["product_name"] ?></h2>
+            <div class="price"><?= $row["price"] ?></div>
             <div class="favourite"><i class='bx bxs-heart'></i></div>
             <div class="details-container"><a href="" class="details">View details</a></div>
         </div>
         <?php
+            }
+        } else {
+            echo "<p>No products found for the selected brand.</p>";
         }
         ?>
     </div>
-    <?php
-    }
-    ?>
     <?php include("footer.php"); ?>
 </body>
 </html>
