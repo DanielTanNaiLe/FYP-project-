fc3b1ae008af01dc582a165a39c52f6a706928a2
DanielTanNaiLe <1211209005@student.mmu.edu.my>
2024-06-03 16:39:50 +0800

    kid

--- LDKsport/Customer/kidpage.php
+++ LDKsport/Customer/kidpage.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
 require '../admin_panel/config/dbconnect.php';
-session_start(); // Ensure session is started
+session_start();
 
 include("header.php");
 
@@ -15,21 +15,21 @@ function displayProducts($result) {
     echo '<div class="listproduct">';
     if ($result->num_rows > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
-?>
-<form action="" method="post" class="box">
-    <input type="hidden" name="pid" value="<?= $row['product_id'];?>">
-    <input type="hidden" name="product_name" value="<?= $row['product_name'];?>">
-    <input type="hidden" name="price" value="<?= $row['price'];?>">
-    <input type="hidden" name="product_image" value="<?= $row['product_image'];?>">
-    <div class="item">
-        <img src="../uploads/<?=$row['product_image'];?>">
-        <h2><?=$row["product_name"];?></h2>
-        <div class="price">RM <?=$row["price"];?></div>
-        <div class="favourite" data-product-id="<?= $row['product_id']; ?>"><i class='bx bxs-heart'></i></div>
-        <div class="details-container"><a href="product_details.php?pid=<?= $row['product_id']; ?>" class="details">View details</a></div>
-    </div>
-</form>
-<?php 
+            ?>
+            <form action="" method="post" class="box">
+                <input type="hidden" name="pid" value="<?= $row['product_id'];?>">
+                <input type="hidden" name="product_name" value="<?= $row['product_name'];?>">
+                <input type="hidden" name="price" value="<?= $row['price'];?>">
+                <input type="hidden" name="product_image" value="<?= $row['product_image'];?>">
+                <div class="item">
+                    <img src="../uploads/<?=$row['product_image'];?>">
+                    <h2><?=$row["product_name"];?></h2>
+                    <div class="price">RM <?=$row["price"];?></div>
+                    <div class="favourite" data-product-id="<?= $row['product_id']; ?>"><i class='bx bxs-heart'></i></div>
+                    <div class="details-container"><a href="product_details.php?pid=<?= $row['product_id']; ?>" class="details">View details</a></div>
+                </div>
+            </form>
+            <?php 
         }
     } else {
         echo '<p>No products found.</p>';
@@ -44,7 +44,7 @@ function displayProducts($result) {
     <link rel="stylesheet" href="general.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script> <!-- Ensure jQuery is loaded -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 </head>
 <body>
     <h1 class="m1">KIDS</h1>
@@ -78,12 +78,15 @@ function displayProducts($result) {
     </div>
     <div class="product-list-container">
         <?php
-        // Fetch and display all products for kids
         $allProductsResult = mysqli_query($conn, "SELECT * FROM product 
                                                   INNER JOIN category ON product.category_id = category.category_id 
                                                   WHERE product.gender_id = (SELECT gender_id FROM gender WHERE gender_name = 'KIDS')");
-        echo '<div class="subtitle_1"><h1>All Kids Products</h1></div>';
-        displayProducts($allProductsResult);
+        if ($allProductsResult === false) {
+            echo '<p>Error executing query: ' . mysqli_error($conn) . '</p>';
+        } else {
+            echo '<div class="subtitle_1"><h1>All Kids Products</h1></div>';
+            displayProducts($allProductsResult);
+        }
         ?>
     </div>
     <?php include("footer.php"); ?>
@@ -132,7 +135,6 @@ function displayProducts($result) {
                 loadProducts('Hats');
             });
 
-            // Load all products by default on page load
             loadProducts('');
         });
     </script>
