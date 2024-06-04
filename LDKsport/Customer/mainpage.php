4fbdebdbdc5ad372c9c40c86dc9e75decf1681dc
DanielTanNaiLe <1211209005@student.mmu.edu.my>
2024-06-04 12:54:03 +0800

    p

--- LDKsport/Customer/mainpage.php
+++ LDKsport/Customer/mainpage.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="general.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 </head>
 <body>
     <?php include("header.php"); ?>
@@ -15,7 +14,7 @@
             <h5>Summer Collection</h5>
             <h1>New Summer <br>Collection 2024</h1>
             <p>There's Nothing like Trend</p>
-            <a href="menpage.html" class="main-btn">Shop now<i class='bx bx-right-arrow-alt'></i></a>
+            <a href="menpage.php" class="main-btn">Shop now<i class='bx bx-right-arrow-alt'></i></a>
         </div>
     </section>
     <section class="main-home2">
@@ -27,9 +26,9 @@
                 <div class="image-container">
                     <img src="./image/banner1.jpg" alt="">
                     <div class="button-overlay">
-                        <a href="#" class="home2-btn" data-category="Men" data-type="Shoes">Men's Shoes</a>
-                        <a href="#" class="home2-btn" data-category="Men" data-type="Clothing">Men's Clothing</a>
-                        <a href="#" class="home2-btn" data-category="Men" data-type="Pants">Men's Pants</a>
+                        <a href="category.php?gender=Men&type=Shoes" class="home2-btn">Men's Shoes</a>
+                        <a href="category.php?gender=Men&type=Clothing" class="home2-btn">Men's Clothing</a>
+                        <a href="category.php?gender=Men&type=Pants" class="home2-btn">Men's Pants</a>
                     </div>
                 </div>
             </div>
@@ -37,9 +36,9 @@
                 <div class="image-container">
                     <img src="./image/banner2.jpg" alt="">
                     <div class="button-overlay">
-                        <a href="#" class="home2-btn" data-category="Girls" data-type="Shoes">Girl's Shoes</a>
-                        <a href="#" class="home2-btn" data-category="Girls" data-type="Clothing">Girl's Clothing</a>
-                        <a href="#" class="home2-btn" data-category="Girls" data-type="Pants">Girl's Pants</a>
+                        <a href="category.php?gender=Girls&type=Shoes" class="home2-btn">Girl's Shoes</a>
+                        <a href="category.php?gender=Girls&type=Clothing" class="home2-btn">Girl's Clothing</a>
+                        <a href="category.php?gender=Girls&type=Pants" class="home2-btn">Girl's Pants</a>
                     </div>
                 </div>
             </div>
@@ -47,9 +46,9 @@
                 <div class="image-container">
                     <img src="./image/banner4.jpg" alt="">
                     <div class="button-overlay">
-                        <a href="#" class="home2-btn" data-category="Kids" data-type="Shoes">Kids' Shoes</a>
-                        <a href="#" class="home2-btn" data-category="Kids" data-type="Clothing">Kids' Clothing</a>
-                        <a href="#" class="home2-btn" data-category="Kids" data-type="Pants">Kids' Pants</a>
+                        <a href="category.php?gender=Kids&type=Shoes" class="home2-btn">Kids' Shoes</a>
+                        <a href="category.php?gender=Kids&type=Clothing" class="home2-btn">Kids' Clothing</a>
+                        <a href="category.php?gender=Kids&type=Pants" class="home2-btn">Kids' Pants</a>
                     </div>
                 </div>
             </div>
@@ -79,33 +78,5 @@
     ?>
     <div class="product-list-container"></div>
     <?php include("footer.php"); ?>
-    <script>
-        $(document).ready(function() {
-            function loadProducts(category, type) {
-                $.ajax({
-                    url: 'fetch_products.php',
-                    method: 'GET',
-                    data: { category: category, type: type },
-                    success: function(response) {
-                        $('.product-list-container').html(response);
-                    },
-                    error: function(xhr, status, error) {
-                        console.error("AJAX Error: ", error);
-                        alert("Failed to load products. Please try again.");
-                    }
-                });
-            }
-
-            $('.home2-btn').click(function(e) {
-                e.preventDefault();
-                var category = $(this).data('category');
-                var type = $(this).data('type');
-                loadProducts(category, type);
-            });
-
-            // Optionally, load a default category on page load
-            loadProducts('Kids', 'Shoes');
-        });
-    </script>
 </body>
 </html>
